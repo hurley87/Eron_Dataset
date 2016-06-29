af6b11ddb4fae6572445401ecafec0390dc8c281
david hurley <davidhurley@davids-MacBook-Air.local>
2016-06-29 14:19:42 -0400

    update analysis

--- README.md
+++ README.md
@@ -1,3 +1,100 @@
-## Investigating Enron financial and email data using machine learning
+## Investigating Enron financial and email data
 David Hurley
 
+### Data Exploration
+	(https://en.wikipedia.org/wiki/Enron_Corpus)[Email Corpus Wiki] 
+	Before Enron filed for bankruptcy in 2001 it was one of the largest energy companies in the world employing approximately 20,000 people and claimed revenues of $111 billion just a year earlier.  This downfall was due to a highly publicized account fraud and corruption scandal. The financial and email data are not public record. 
+
+	The purpose of this project is to build a person of interest identifier based on financial and email data made public as a result of the Enron scandal. The folks at Udacity have been nice enough to put all the relevant data is a dictionary which contains 14 financial features ('salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees']), 6 email features ('to_messages', 'email_address', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi'), and a POI label.
+
+	A machine learning analysis would be beneficial to understand some of the causes of this type of fraud. The features that have the most impact would give us insight into the motivations behind those employees that were corrupted. Similar models may even be used in the future to detect and hopefully prevent something like this happening again. 
+
+	Of the 146 data points 18 are labeled as a POI which is not a significant amount of POI data. Printing out the complete data set I see that there are many values that are NaN. I removed these data points as needed which I will dicuss in the next section on outliers. 
+
+
+### Outlier Investigation
+	For outliers I started by visualizing salary versus bonus and there is one clear outlier. I found this data point by excluding people who had a bonus of NaN and finding the max value which belonged to 'TOTAL'. Obviously it makes sense to exclude this data going forward. 
+
+	I then looked at the top salaries while excluding those salaries that were NaN and came across two key people as listed on the Enron wiki page, https://en.wikipedia.org/wiki/Enron_scandal. While these are outliers it made sense not to exclude them. 
+
+
+### New Features
+	I decided to add two new features to the data set, the ratio of emails a person recieved from a POI (ratio_from_poi) and the ratio of emails sent to a POI (ratio_to_poi). Intuitively if most of your email exchange is with a POI you most likely at least know about the scandal and are a POI yourself. I started by visualizing the relationship between from_messages and to_messages and saw that people typically recieve many more emails then they send. Taking the ratio here makes sense because we view the data in a scale between 0 - 1. 
+
+### Selected Features
+	I found this to be the hardest part of the assignment. At first I started with features I started with only features I thought we relevant like those related to stock, earnings, and correspondance with POIs. I originally tried SVM, Naive Bayes, and a Decision Tree classifiers. Supervised learning algorithms makes sense here because we know beforehand who is a POI.  For each I checked the accurancy, percision and recall of each. I did not find a combination of features where SVM had a percision and recall greater then 3 so I disregarded it almost right away.
+
+	I then used a recursive approach to finding the most important features using feature_importances_ to rank each feature. I started with all features and then would remove features when they had an importance of 0. Each I did this accurancy, recall and percision increased after each iteration until it didn't. In my analysis we can see each iteration. 
+
+	###First Iteration
+	Naive Bayes accuracy 0.333333333333
+	precision =  0.2
+	recall =  0.5
+
+	DecisionTreeClassifier
+	Decision Tree accuracy 0.666666666667
+	precision =  0.333333333333
+	recall =  0.25
+
+	###Second Iteration
+	GaussianNB
+	Naive Bayes accuracy 0.8
+	precision =  1.0
+	recall =  0.25
+
+	DecisionTreeClassifier
+	Decision Tree accuracy 0.866666666667
+	precision =  0.75
+	recall =  0.75
+
+	###Third Iteration
+	GaussianNB
+	Naive Bayes accuracy 0.8
+	precision =  1.0
+	recall =  0.25
+
+	DecisionTreeClassifier
+	Decision Tree accuracy 0.8
+	precision =  0.6
+	recall =  0.75
+
+	###Fourth Iteration
+	GaussianNB
+	Naive Bayes accuracy 0.8
+	precision =  1.0
+	recall =  0.25
+
+	DecisionTreeClassifier
+	Decision Tree accuracy 0.8
+	precision =  1.0
+	recall =  0.25
+
+	Looking at the results above the second iteration decision tree classifier seems most promising. An accuracy of 0.87, percision of 0.75 and recall of 0.75. In each iteration the Decision Tree Classifier out preformed GaussianNB so it made sense to work with the DecisionTreeClassifier for the rest of the analysis. 
+
+### Feature Scaling
+	No feature scaling was necessary because I decided to use the Decision Tree Algorithm for the POI identifier. 
+
+### Tune the Algorithm
+	I tried tuning the Decision Tree Algorithm by using different values for min_sample_split. This had little effect of the values I had previously,
+
+	Decision Tree accuracy 0.866666666667
+	precision =  0.75
+	recall =  0.75 
+
+	I didn't think much of it until I tried to use KFold to split and validate algorithm. I discovered that I had recall and precision below 3 which was frustrating. 
+
+### Validate and Evaluate 
+	Initially I tried to validate using 3-fold cross-validation of precision and recall scores and found that scores were under 3 so go back and change my approach to picking the right features. 
+
+	accuracy before tuning  0.857142857143
+	precision after tuning  0.4
+	recall after tuning  0.666666666667
+
+	 
+	accuracy after tuning  0.892857142857
+	precision after tuning  0.4
+	recall after tuning  0.666666666667
+
+
+	Accuracy: 0.86256	Precision: 0.37669	Recall: 0.36200	F1: 0.36920	F2: 0.36485
+	Total predictions: 9000	True positives:  362	False positives:  599	False negatives:  638	True negatives: 7401
--- poi_id.py
+++ poi_id.py
@@ -3,9 +3,15 @@
 import sys
 import pickle
 sys.path.append("./tools/")
-
+import matplotlib.pyplot
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
@@ -15,43 +21,232 @@ features_list = ['poi','salary', 'to_messages'] # You will need to use more feat
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
+    # general data points related to pois and non-pois
     print 'Total number of data points:'
-    print len(data_dict.keys())
+    total = len(data_dict.keys())
+    print total
+    poi_total = len([person['poi'] for person in data_dict.values() if person['poi']])
+    print 'Number of POIs'
+    print poi_total
+    print 'Number of Non-POIs'
+    non_pois = total - poi_total
+    print non_pois
 
-### Task 2: Remove outliers
+### Task 2: Remove outliers 
+features = ["poi", "salary", "bonus"]
+data = featureFormat(data_dict, features)
+
+def viewScatter(data, features):
+	for point in data:
+	    x = point[1]
+	    y = point[2]
+	    matplotlib.pyplot.scatter( x, y )
+	    if point[0] == 1:
+        	matplotlib.pyplot.scatter(x, y, color="r")
+	matplotlib.pyplot.xlabel(features[1])
+	matplotlib.pyplot.ylabel(features[2])
+	matplotlib.pyplot.show()
+
+def featureOutliers(data_dict, feature, amount):
+	outliers = []
+	for key in data_dict:
+	    val = data_dict[key][feature]
+	    # skip NaN's
+	    if val == 'NaN':
+	        continue
+	    outliers.append((key, int(val)))
+
+	return (sorted(outliers, key=lambda x:x[1], reverse=True)[:amount])
+
+# viewScatter(data, features)
+
+print 'Salary outlier:'
+# print top salary
+print featureOutliers(data_dict, 'salary', 1)
+
+# delete TOTAL as it wont be needed for the analysis
+data_dict.pop('TOTAL', 0)
+data = featureFormat(data_dict, features)
+
+# view data again
+# viewScatter(data, features)
+print 'Salary outliers'
+
+# top 3 salaries
+print featureOutliers(data_dict, 'salary', 3)
+
+
 ### Task 3: Create new feature(s)
+
+features = ["pois", "to_messages", "from_messages"]
+# viewScatter(data, features)
+
+# use this to create new 
+def new_list(key, attribute):
+    new_list=[]
+
+    for i in data_dict:
+        if data_dict[i][key] == "NaN" or data_dict[i][attribute] == "NaN":
+            new_list.append(0.)
+        elif data_dict[i][key] >= 0:
+            new_list.append(float(data_dict[i][key])/float(data_dict[i][attribute]))
+
+    return new_list
+
+
+
+# create new features,ratio of emails from and to a POI
+ratio_from_poi = new_list("from_poi_to_this_person", "to_messages")
+ratio_to_poi = new_list("from_this_person_to_poi", "from_messages")
+
+# add new features to each data point
+count = 0
+for i in data_dict:
+    data_dict[i]["ratio_from_poi"] = ratio_from_poi[count]
+    data_dict[i]["ratio_to_poi"] = ratio_to_poi[count]
+    count += 1
+
+features_list = ["poi", "ratio_from_poi", "ratio_to_poi"] 
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
 ### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True)
+data = featureFormat(my_dataset, features_list)
 labels, features = targetFeatureSplit(data)
 
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
+# viewScatter(data, features)
 
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+
+### Task 4: Try a varity of classifiers
+
+def run_classifiers(features_list):
+	data = featureFormat(my_dataset, features_list)
+	labels, features = targetFeatureSplit(data)   
+
+	from sklearn import cross_validation
+	features_train, features_test, labels_train, labels_test = \
+		cross_validation.train_test_split(features, labels, test_size=0.1, random_state=42)
+
+	### try Naive Bayes for prediction
+	print '\nGaussianNB'
+	clf = GaussianNB()
+	clf.fit(features_train, labels_train)
+	pred = clf.predict(features_test)
+	accuracy = accuracy_score(pred, labels_test)
+	print 'Naive Bayes accuracy', accuracy
+	print 'precision = ', precision_score(labels_test,pred)
+	print 'recall = ', recall_score(labels_test,pred)
+
+	print '\nDecisionTreeClassifier'
+	clf = DecisionTreeClassifier(min_samples_split=6)
+	clf.fit(features_train,labels_train)
+	score = clf.score(features_test,labels_test)
+	pred = clf.predict(features_test)
+	print 'Decision Tree accuracy', score
+	print 'precision = ', precision_score(labels_test,pred)
+	print 'recall = ', recall_score(labels_test,pred)
+
+	print '\nFeature importance: '
+	importances = clf.feature_importances_
+	import numpy as np
+	for i in range(len(features_list)-2):
+		if importances[i+1] != 0:
+			print "{}: {}".format(features_list[i+1], importances[i+1])	
+
+	return clf
+
+print '\nfirst iteration' 
+features_list = ["poi", "ratio_from_poi", "ratio_to_poi", 'salary', 
+				'deferral_payments', 'total_payments', 'loan_advances', 
+				'bonus', 'restricted_stock_deferred', 'deferred_income', 
+				'total_stock_value', 'expenses', 'exercised_stock_options', 
+				'other', 'long_term_incentive', 'restricted_stock', 'director_fees', 
+				'to_messages', 'from_poi_to_this_person', 'from_messages', 
+				'from_this_person_to_poi', 'shared_receipt_with_poi']		
+
+clf = run_classifiers(features_list)
+
+print '\nsecond iteration'
+features_list = ["poi", "ratio_from_poi", 'salary','total_payments', 'deferred_income', 
+				'total_stock_value', 'expenses', 'long_term_incentive']
+
+clf = run_classifiers(features_list)
+
+print '\nthird iteration'
+features_list = ["poi", 'salary','total_payments', 'deferred_income', 
+				'total_stock_value', 'expenses']
+
+clf = run_classifiers(features_list)
+
+print '\nfourth iteration'
+features_list = ["poi", 'salary','total_payments', 'deferred_income', 
+				'total_stock_value', 'expenses']
+
+clf = run_classifiers(features_list)
+		
+print '\nfifth iteration'
+features_list = ["poi", 'salary','total_payments', 'deferred_income']
+
+clf = run_classifiers(features_list)
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+### using our testing script. 
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
+features_list = ["poi", 'ratio_to_poi', 'shared_receipt_with_poi', 'ratio_from_poi']
+
+data = featureFormat(my_dataset, features_list)
+labels, features = targetFeatureSplit(data)  
+
+
+from sklearn import cross_validation
 features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+	cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)	
+
+# try different min_samples_split to see the effect on accuracy, precision and recall
+for i in range(4):
+	print i
+	clf = DecisionTreeClassifier(min_samples_split=i+1)
+	clf.fit(features_train,labels_train)
+	score = clf.score(features_test,labels_test)
+	pred = clf.predict(features_test)
+	print 'Decision Tree accuracy', score
+	print 'precision = ', precision_score(labels_test, pred)
+	print 'recall = ', recall_score(labels_test,pred)	
+
+
+# use KFold for split and validate algorithm
+from sklearn.cross_validation import KFold
+kf=KFold(len(labels),3)
+for train_indices, test_indices in kf:
+    #make training and testing sets
+    features_train= [features[ii] for ii in train_indices]
+    features_test= [features[ii] for ii in test_indices]
+    labels_train=[labels[ii] for ii in train_indices]
+    labels_test=[labels[ii] for ii in test_indices]
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+score = clf.score(features_test,labels_test)
+pred= clf.predict(features_test)
+print 'accuracy before tuning ', score
+print 'precision after tuning ', precision_score(labels_test,pred)
+print 'recall after tuning ', recall_score(labels_test,pred)
+
+
+print '\n '
+clf = DecisionTreeClassifier(min_samples_split=3, criterion='entropy')
+clf.fit(features_train,labels_train)
+score = clf.score(features_test,labels_test)
+print 'accuracy after tuning ', score
+print 'precision after tuning ', precision_score(labels_test,pred)
+print 'recall after tuning ', recall_score(labels_test,pred)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
-
+clf = DecisionTreeClassifier(min_samples_split=3, criterion='entropy')
+features_list = ["poi", 'ratio_to_poi', 'shared_receipt_with_poi', 'ratio_from_poi']
+data = featureFormat(my_dataset, features_list)
 dump_classifier_and_data(clf, my_dataset, features_list)
